a535cb70f4c4b1801b33fe23fcf1b8736821b8d3
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:59 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# linked-list-implementation
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Linked List Data Structure Implementation in Python")
+    print("Project: linked-list-implementation")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
